2ae6818c8205f414c00d753fc5ed4429d2a9900f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-24 23:18:49 +0300

    build(core): impl WebFlux

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ repositories {
 dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
