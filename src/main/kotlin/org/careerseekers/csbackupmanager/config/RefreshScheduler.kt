d2170f53c3469ef7bc33cc9ba4112ec13d5efc72
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-24 16:36:38 +0300

    feat(core): created Refreshable annotation & BPP

---
+++ src/main/kotlin/org/careerseekers/csbackupmanager/config/RefreshScheduler.kt
@@ -0,0 +1,37 @@
+package org.careerseekers.csbackupmanager.config
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class RefreshScheduler(
+    private val context: ApplicationContext,
+    private val refreshableBeanPostProcessor: RefreshableClassesBeanPostProcessor
+) {
+    private val logger = LoggerFactory.getLogger(RefreshScheduler::class.java)
+    private val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+
+    @Scheduled(fixedRate = 60_000)
+    fun refreshBeans() {
+        val now = System.currentTimeMillis()
+
+        refreshableBeanPostProcessor.refreshableBeans.forEach { (beanName, info) ->
+            if (now - info.lastRefreshTime > info.refreshPeriod) {
+                coroutineScope.launch {
+                    val newInstance = context.getBean(beanName)
+                    refreshableBeanPostProcessor.refreshableBeans[beanName] = info.copy(
+                        lastRefreshTime = System.currentTimeMillis(),
+                        currentBeanInstance = newInstance
+                    )
+                    logger.info("Bean $beanName was refreshed in coroutine")
+                }
+            }
+        }
+    }
+}
