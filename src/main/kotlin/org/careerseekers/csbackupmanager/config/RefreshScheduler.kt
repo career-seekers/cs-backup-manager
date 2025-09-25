c2ff0226631bce9fc359c1f168c80fbc97c9256c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-25 13:22:03 +0300

    feat(core): updated beans scheduler, set beans scope to Prototype

--- src/main/kotlin/org/careerseekers/csbackupmanager/config/RefreshScheduler.kt
+++ src/main/kotlin/org/careerseekers/csbackupmanager/config/RefreshScheduler.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.DisposableBean
 import org.springframework.context.ApplicationContext
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -22,7 +23,12 @@ class RefreshScheduler(
         val now = System.currentTimeMillis()
 
         refreshableBeanPostProcessor.refreshableBeans.forEach { (beanName, info) ->
+            val oldInstance = info.currentBeanInstance
             if (now - info.lastRefreshTime > info.refreshPeriod) {
+                if (oldInstance is DisposableBean) {
+                    oldInstance.destroy()
+                }
+
                 coroutineScope.launch {
                     val newInstance = context.getBean(beanName)
                     refreshableBeanPostProcessor.refreshableBeans[beanName] = info.copy(
--- src/main/kotlin/org/careerseekers/csbackupmanager/services/EventsServicePostgresBackupService.kt
+++ src/main/kotlin/org/careerseekers/csbackupmanager/services/EventsServicePostgresBackupService.kt
@@ -4,21 +4,26 @@ import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.careerseekers.csbackupmanager.annotations.Refreshable
 import org.careerseekers.csbackupmanager.config.properties.EventsServiceDatabaseConfigurationProperties
 import org.careerseekers.csbackupmanager.enums.DatabaseNames
+import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Service
 import java.io.File
 
 @Service
 @Refreshable
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 class EventsServicePostgresBackupService(
     override val databaseProperties: EventsServiceDatabaseConfigurationProperties,
     override val yandexDiskService: YandexDiskService,
-) : IPostgresBackupService {
+) : IPostgresBackupService, DisposableBean {
 
     @Value("\${storage-dir}")
     private lateinit var storagePath: String
@@ -38,5 +43,9 @@ class EventsServicePostgresBackupService(
                 database = database
             ).awaitSingleOrNull()
         }
+    }
+
+    override fun destroy() {
+        coroutineScope.cancel()
     }
 }
